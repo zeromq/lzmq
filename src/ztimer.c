a09d9821e5ce4799b0d097afab70381cda555544
moteus <mimir@newmail.ru>
2013-05-09 09:56:11 +0400

    Fix. use clock_gettime.

--- src/ztimer.c
+++ src/ztimer.c
@@ -144,7 +144,7 @@ monotonic_time_t GetMonotonicTime(){
 #ifdef USE_CLOCK_MONOTONIC
   struct timespec ts;
   if(0 == clock_gettime(CLOCK_MONOTONIC, &ts))
-    return (monotonic_time_t)ts.tv_sec + ts.tv_usec / (monotonic_time_t)(1000000000);
+    return (monotonic_time_t)ts.tv_sec + ts.tv_nsec / (monotonic_time_t)(1000000000);
 #endif
   return GetUtcTime();
 }
