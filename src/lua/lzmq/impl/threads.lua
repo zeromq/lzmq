19a7c67abee040afc4437acebdce2cf8e1fce71a
Alexey Melnichuk <mimir@newmail.ru>
2014-07-02 14:22:20 +0500

    Add. Licence and Copyright notes to source files. [ci skip]

--- src/lua/lzmq/impl/threads.lua
+++ src/lua/lzmq/impl/threads.lua
@@ -1,4 +1,14 @@
 --
+--  Author: Alexey Melnichuk <mimir@newmail.ru>
+--
+--  Copyright (C) 2013-2014 Alexey Melnichuk <mimir@newmail.ru>
+--
+--  Licensed according to the included 'LICENCE' document
+--
+--  This file is part of lua-lzqm library.
+--
+
+--
 -- zmq.thread wraps the low-level threads object & a zmq context.
 --
 
--- src/zcontext.h
+++ src/zcontext.h
@@ -1,3 +1,13 @@
+/*
+  Author: Alexey Melnichuk <mimir@newmail.ru>
+
+  Copyright (C) 2013-2014 Alexey Melnichuk <mimir@newmail.ru>
+
+  Licensed according to the included 'LICENCE' document
+
+  This file is part of lua-lzqm library.
+ */
+
 #ifndef _ZCONTEXT_H_
 #define _ZCONTEXT_H_
 
