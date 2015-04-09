1326b4d4f8b4493023e35c4eb22c9463bfbf2012
Alexey Melnichuk <mimir@newmail.ru>
2015-04-09 13:54:30 +0400

    Update LibUV example.[ci skip]

--- examples/uv_actor.lua
+++ examples/uv_actor.lua
@@ -1,33 +1,64 @@
 -- Use LibUV event loop in actor thread
---
--- !NOTE! I can not figure out why this does not work
---  with `inproc` transport so I use `tcp` to communicate
---  with actor thread
 
 local function thread(pipe)
   local uv  = require "lluv"
+  local ut  = require "lluv.utils"
   local zmq = require "lzmq"
 
-  local function uv_poll_zmq(sock, cb)
-    uv.poll_socket(sock:fd()):start(function(handle, err, event)
-      if err then cb(sock, err) else
-        while handle:active() and not sock:closed() do
-          local ok, err = sock:has_event(zmq.POLLIN) 
-          if ok == nil then cb(sock, err) break end
-          if ok then cb(sock) else break end
-        end
-      end
-      if sock:closed() then handle:close() end
-    end)
+  local uv_poll_zmq = ut.class() do
+
+  function uv_poll_zmq:__init(s)
+    self._s = s
+    self._h = uv.poll_socket(s:fd())
+    return self
   end
 
-  uv_poll_zmq(pipe, function(pipe, err)
-    if err then
-      print("Poll error:", err)
-      return pipe:close()
+  local function on_poll(self, err, cb)
+    if err then cb(self, err, self._s) else
+      while self._h:active() and not self._s:closed() do
+        local ok, err = self._s:has_event(zmq.POLLIN)
+        if ok == nil then cb(self, err, self._s) break end
+        if ok then cb(self, nil, self._s) else break end
+      end
+    end
+    if self._s:closed() then self._h:close() end
+  end
+
+  function uv_poll_zmq:start(cb)
+    self._h:start(function(handle, err) on_poll(self, err, cb) end)
+
+    -- For `inproc` socket without this call socket never get in signal state.
+    local ok, err = self._s:has_event(zmq.POLLIN)
+    if ok == nil then
+      -- context already terminated
+      uv.defer(on_poll, self, err, cb)
+    elseif ok then
+      -- socket already has events
+      uv.defer(on_poll, self, nil, cb)
     end
 
-    print(pipe:recv())
+    return self
+  end
+
+  function uv_poll_zmq:stop()
+    self._h:stop()
+    return self
+  end
+
+  function uv_poll_zmq:close(...)
+    self._h:close(...)
+    return self
+  end
+
+  end
+
+  uv_poll_zmq.new(pipe):start(function(handle, err, pipe)
+    if err then
+      print("Poll error:", err)
+      return handle:close()
+    end
+
+    print("Pipe recv:", pipe:recvx())
   end)
 
   uv.timer():start(1000, function()
@@ -40,7 +71,7 @@ end
 local zth = require "lzmq.threads"
 local ztm = require "lzmq.timer"
 
-local actor = zth.xactor{thread, pipe = 'tcp'}:start()
+local actor = zth.xactor(thread):start()
 
 for i = 1, 5 do
   actor:send("Hello #" .. i)
